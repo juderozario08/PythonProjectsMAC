3a6aae66b9f075c6020e296000d16a865a01ee8a
Jude Rozario <juderozario@Judes-MacBook-Air.local>
2022-10-24 21:10:17 -0400

    First Commit

--- waterlooContest/modernArt.py
+++ waterlooContest/modernArt.py
@@ -1,37 +1,25 @@
-rows = int(input())
-columns = int(input())
-num_of_changes = int(input())
-row_changes = []
-column_changes = []
-canvas = []
-for i in range(num_of_changes):
-    change = str(input())
-    if change[0] == "R":
-        change = int(change[2:])
-        row_changes.append(change)
+rows, columns = int(input()), int(input())
+canvas = [0]*columns*rows
+for i in range(int(input())):
+    string = input().split(" ")
+    index = int(string[1])-1
+    if string[0] == 'R':
+        for i in range(columns):
+            ind = i + (index * columns)
+            canvas[ind] += 1
     else:
-        change = int(change[2:])
-        column_changes.append(change)
-for i in range(rows):
-    result = []
-    for j in range(columns):
-        result.append(True)
-    canvas.append(result)
-for i in row_changes:
-    for j in range(columns):
-        if canvas[i-1][j] is True:
-            canvas[i-1][j] = False
-        else:
-            canvas[i-1][j] = True
-for i in column_changes:
-    for j in range(rows):
-        if canvas[j][i-1] is True:
-            canvas[j][i-1] = False
-        else:
-            canvas[j][i-1] = True
-counter = 0
-for i in range(rows):
-    for j in canvas[i]:
-        if j is False:
-            counter += 1
-print(counter)
+        for i in range(rows):
+            ind = index + (i * columns)
+            canvas[ind] += 1
+print(sum([1 for i in canvas if i % 2 == 1]))
+
+
+# row, col, numOfChanges = int(input()), int(input()), int(input())
+# rows = set()
+# columns = set()
+# for i in range(numOfChanges):
+#     change = input().split(" ")
+#     (rows.remove(change[1]) if change[1] in rows else rows.add(
+#         change[1])) if change[0] == 'R' else (columns.remove(change[1]) if change[1] in columns else columns.add(change[1]))
+#     # Number of changes in rows + Number of actual changes in columns -
+# print(row * len(columns) + col * len(rows) - 2 * len(columns) * len(rows))
